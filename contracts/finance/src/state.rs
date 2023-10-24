f688057b8e9dc38d47aa762cdfae3c674aaff2c2
Łukasz Ptak <StylishTriangles@users.noreply.github.com>
2023-10-24 17:49:11 +0200

    State vars

--- contracts/finance/src/state.rs
+++ contracts/finance/src/state.rs
@@ -1,8 +1,8 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{Addr, Coin};
-use cw_storage_plus::Item;
+use cosmwasm_std::{Addr, Coin, Uint128};
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
@@ -15,3 +15,33 @@ pub struct State {
 
 pub const CONFIG_KEY: &str = "config";
 pub const CONFIG: Item<State> = Item::new(CONFIG_KEY);
+
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct UserAssetInfo {
+    pub collateral: Uint128,
+    pub loan_amount: Uint128,
+    pub cumulative_interest: Uint128,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct UserData {
+    pub last_update_time: u64,
+    pub ltv: Uint128,
+    pub assets_value: Uint128,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct AssetInfo {
+    /// For FE
+    pub apr: Uint128,
+    pub total_dep: Uint128,
+    pub total_fasset: Uint128,
+    pub cumulative_interest: Uint128,
+}
+
+
+pub const USER_ASSET: Map<(&Addr, &str), UserAssetInfo> = Map::new("user_asset");
+pub const USER_DATA: Map<&Addr, UserData> = Map::new("user_data");
+pub const ASSET_INFO: Map<&str, AssetInfo> = Map::new("asset_info");
+pub const ASSETS: Item<Vec<String>> = Item::new("assets");
