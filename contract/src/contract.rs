5191606262b20b19176f0b62b8b12b891fc10a14
Pawel Rejkowicz <pawel@btblock.io>
2023-10-24 15:19:48 +0200

    testing maps

--- contract/src/contract.rs
+++ contract/src/contract.rs
@@ -1,11 +1,14 @@
 use cosmwasm_std::{
-    entry_point, to_binary, BankMsg, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+    entry_point, to_binary, BankMsg, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Addr, Uint128,
 };
+use cw_storage_plus::Map;
 
 use crate::error::ContractError;
 use crate::msg::{ConfigResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::{State, CONFIG};
 
+
+pub const AMOUNTS: Map<Addr, Uint128> = Map::new("amounts");
 #[entry_point]
 pub fn instantiate(
     deps: DepsMut,
@@ -23,6 +26,7 @@ pub fn execute(
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
+    AMOUNTS.save(deps.storage, info.sender, &Uint128::new(13))?;
     Ok(Response::default())
 }
 
