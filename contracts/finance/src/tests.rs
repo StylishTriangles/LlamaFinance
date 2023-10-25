a06a4fa2fd2dd7f372a48fb1d8582f30b0714d1a
Pawel Rejkowicz <pawel@btblock.io>
2023-10-25 21:34:49 +0200

    tests in progress

--- contracts/finance/src/tests.rs
+++ contracts/finance/src/tests.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use cosmwasm_std::{testing::{mock_dependencies_with_balances, mock_env, mock_info}, Addr};
+use cosmwasm_std::{testing::{mock_dependencies_with_balances, mock_env, mock_info}, Addr, Coin, Uint128};
 
 use crate::{msg::{InstantiateMsg, ExecuteMsg}, contract::{instantiate, execute}, state::RATE_DENOMINATOR};
 
@@ -12,10 +12,16 @@ fn rate(x: u32) -> u32 {
 fn first() -> Result<(), String> {
     let oracle: String = Addr::unchecked("oracle").into();
     let creator: String = Addr::unchecked("creator").into();
-    let balances = &[
-        
+    let alice: String = Addr::unchecked("alice").into();
+    let btc: String = Addr::unchecked("btc").into();
+    let usdc: String = Addr::unchecked("btc").into();
+    let alice_balances = &[
+        Coin { denom: btc.clone(), amount: Uint128::new(10_000), },
     ];
-    let mut deps = mock_dependencies_with_balances(balances);
+    let balances = [
+        (alice.as_str(), alice_balances),
+    ];
+    let mut deps = mock_dependencies_with_balances(&balances);
     {
         let msg = InstantiateMsg {
             oracle: oracle.clone(),
@@ -25,7 +31,6 @@ fn first() -> Result<(), String> {
         let info = mock_info(&creator, &[]);
         instantiate(deps.as_mut(), env, info, msg).map_err(|e|format!("instantiate: {}", e))?;
     }
-    let btc: String = Addr::unchecked("btc").into();
     let btc_rate = rate(80);
     let btc_min_rate = rate(10);
     let btc_optimal_rate = rate(50);
@@ -43,7 +48,6 @@ fn first() -> Result<(), String> {
         let info = mock_info(&creator, &[]);
         execute(deps.as_mut(), env, info, msg).map_err(|e|format!("add asset btc: {}", e))?;
     }
-    let usdc: String = Addr::unchecked("btc").into();
     let usdc_rate = rate(80);
     let usdc_min_rate = rate(10);
     let usdc_optimal_rate = rate(50);
@@ -60,6 +64,14 @@ fn first() -> Result<(), String> {
         let env = mock_env();
         let info = mock_info(&creator, &[]);
         execute(deps.as_mut(), env, info, msg).map_err(|e|format!("add asset usdc: {}", e))?;
+    }
+    {
+        let msg = ExecuteMsg::DepositCollateral {};
+        let env = mock_env();
+        let info = mock_info(&alice, &[
+            Coin { denom: btc.clone(), amount: Uint128::new(100)},
+        ]);
+        execute(deps.as_mut(), env, info, msg).map_err(|e|format!("deposit collateral: {}", e))?;
     }
 
 
