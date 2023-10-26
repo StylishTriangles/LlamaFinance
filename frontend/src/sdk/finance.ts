6e67171452ab5199a1471fe5a0a9f9c8bd72464a
Łukasz Ptak <StylishTriangles@users.noreply.github.com>
2023-10-26 02:48:27 +0200

    hella

--- frontend/src/sdk/finance.ts
+++ frontend/src/sdk/finance.ts
@@ -285,11 +285,15 @@ export class Finance {
     let totalCollateralUSD = 0;
     let totalBorrowUSD = 0;
     for (const uai of data.values()) {
-      if (uai.denom === denom)
-        totalCollateralUSD += (uai.collateral + delta) * uai.price_per_unit * uai.precision;
-      else
-        totalCollateralUSD += uai.collateralUSD;
-      totalBorrowUSD += uai.borrowAmountUSD;
+      if (uai.denom === denom) {
+        if (delta < 0) {
+          totalCollateralUSD += uai.collateralUSD;
+          totalBorrowUSD += (uai.borrowAmount - delta) * uai.price_per_unit * uai.precision;
+        } else {
+          totalCollateralUSD += (uai.collateral + delta) * uai.price_per_unit * uai.precision;
+          totalBorrowUSD += uai.borrowAmountUSD;
+        }
+      }
     }
     if (totalCollateralUSD < 1e-6)
       return 0;
