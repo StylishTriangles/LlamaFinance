3f04ab3b6f2093b1d409a86b526ed8b7e71dca0f
Patryqss <gpatryk14@gmail.com>
2023-10-26 02:09:44 +0200

    Not connected info

--- frontend/src/layouts/index.vue
+++ frontend/src/layouts/index.vue
@@ -7,7 +7,19 @@ import Navbar from "~/components/Navbar.vue";
     <div class="min-h-screen bg-gradient-to-bl from-primary to-base-100">
       <div class="mx-auto max-w-7xl pb-20">
         <Navbar />
-        <RouterView />
+        <RouterView v-if="accountStore.walletAddress" />
+        <div v-else class="flex flex-col items-center gap-20 py-20 text-primary-content">
+          <div class="card w-108">
+            <div class="card-body">
+              <h2 class="card-title text-4xl">
+                Not Connected!
+              </h2>
+              <p class="text-xl">
+                Please connect with your Keplr wallet in order to use LlamaFinance
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
